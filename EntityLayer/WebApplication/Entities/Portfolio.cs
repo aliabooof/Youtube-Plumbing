2635a81ab0beb3ac36d978923c952196b6e83abc
Ali Abooof <aliabooof@outlook.com>
2025-01-24 17:03:16 +0200

    Creating Entities

---
+++ EntityLayer/WebApplication/Entities/Portfolio.cs
@@ -0,0 +1,21 @@
+﻿using CoreLayer.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.WebApplication.Entities
+{
+    public class Portfolio:BaseEntity
+    {
+        public string Title { get; set; } = null!;
+        public string FileName { get; set; } = null!;
+        public string FileType { get; set; } = null!;
+
+        
+        public int CategoryId {  get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
